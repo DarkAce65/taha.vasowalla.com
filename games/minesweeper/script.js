fdc41eacaed6722eeeb989f79e3a06d99721c439
DarkAce65 <tvdood99@gmail.com>
2016-08-15 13:50:47 -0400

    Create openCell function
    Former-commit-id: f5b1c2d5c96ed270fa3d1c2e2bf9a29b2033ad03

--- games/minesweeper/script.js
+++ games/minesweeper/script.js
@@ -1,6 +1,33 @@
 "use strict";
 
 $(function() {
+	function openCell(row, col)  {
+		if(minefield[row][col].state === "closed") {
+			minefield[row][col].state = "open";
+			var value = minefield[row][col].value;
+			var cell = $(".cell[data-row=" + row + "][data-col=" + col + "]");
+			if(value < 0) {
+				cell.removeClass("closed");
+				cell.addClass("mine");
+			}
+			else {
+				if(value === 0) {
+					var rowStart = Math.max(0, row - 1);
+					var rowEnd = Math.min(minefield.length - 1, row + 1);
+					var colStart = Math.max(0, col - 1);
+					var colEnd = Math.min(minefield[0].length - 1, col + 1);
+					for(var r = rowStart; r <= rowEnd; r++) {
+						for(var c = colStart; c <= colEnd; c++) {
+							openCell(r, c);
+						}
+					}
+				}
+				cell.removeClass("closed");
+				cell.addClass("open" + value);
+			}
+		}
+	}
+
 	function addMines(numMines) {
 		var mineLocations = [];
 		for(var i = 0; i < numMines; i++) { // Reservoir sampling
@@ -39,7 +66,7 @@ $(function() {
 			for(var c = 0; c < cols; c++) {
 				minefield[r][c] = {
 					value: 0, // 0 to 8, negative = mine
-					state: "none" // flag,
+					state: "closed" // open, closed, flag
 				};
 			}
 		}
@@ -53,7 +80,7 @@ $(function() {
 		for(var r = 0; r < minefield.length; r++) {
 			board += "<tr>";
 			for(var c = 0; c < minefield[r].length; c++) {
-				board += '<td data-row="' + r + '" data-col="' + c + '" class="cell"></td>';
+				board += '<td data-row="' + r + '" data-col="' + c + '" class="cell closed"></td>';
 			}
 			board += "</tr>";
 		}
@@ -62,12 +89,7 @@ $(function() {
 		$(".cell").click(function(e) {
 			var r = $(e.target).data("row");
 			var c = $(e.target).data("col");
-			if(minefield[r][c].value < 0) {
-				$(e.target).addClass("mine");
-			}
-			else {
-				$(e.target).addClass("open" + minefield[r][c].value);
-			}
+			openCell(r, c);
 		});
 
 		$(".cell").contextmenu(function(e) {
@@ -75,12 +97,12 @@ $(function() {
 			var r = $(e.target).data("row");
 			var c = $(e.target).data("col");
 			switch(minefield[r][c].state) {
-				case "none":
+				case "closed":
 					minefield[r][c].state = "flag";
 					$(e.target).addClass("flag");
 					break;
 				case "flag":
-					minefield[r][c].state = "none";
+					minefield[r][c].state = "closed";
 					$(e.target).removeClass("flag");
 					break;
 			}
