017a1ac29adfef506c1922139413ce1afc1ed5ef
DarkAce65 <tvdood99@gmail.com>
2016-11-12 01:55:07 -0500

    Add origami crane model and cube with morphing
    Former-commit-id: a53d4fe90e860b3bf41af12f9b0ea114a881475d

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+"use strict";
+
 window.requestAnimFrame =
 	window.requestAnimationFrame ||
 	window.webkitRequestAnimationFrame ||
@@ -16,117 +18,63 @@ $(function() {
 		}
 	}
 
-	var factor = window.innerWidth < 768 ? 1.5 : 1;
 	var animation = {planetRotation: 1, satelliteOrbit: 1};
 
 	var clock = new THREE.Clock();
 	var scene = new THREE.Scene();
 	var renderer = new THREE.WebGLRenderer({antialias: true});
 	$("#rendererContainer").append(renderer.domElement);
-	renderer.setSize(window.innerWidth, window.innerHeight);
+	var width = Math.max(window.innerWidth, 100);
+	var height = Math.max(window.innerHeight, 100);
+	renderer.setSize(width, height);
 	renderer.setClearColor(0x131d29);
 	renderer.setPixelRatio(window.devicePixelRatio);
 
-	var camera = new THREE.PerspectiveCamera(35, window.innerWidth / window.innerHeight, 0.1, 10000);
-	camera.position.set(-50, -300, 100);
+	var camera = new THREE.PerspectiveCamera(35, width / height, 0.1, 10000);
+	camera.position.set(0, 0, 100);
 	scene.add(camera);
+	var controls = new THREE.TrackballControls(camera, renderer.domElement);
 	camera.lookAt(scene.position);
 
-	var lights = [];
+	var ambient = new THREE.AmbientLight({color: 0xcccccc});
+	scene.add(ambient);
 
-	// lights[0] = new THREE.PointLight(0xffffff, 3);
+	var objectsLeft = 1;
 
-	lights[0] = new THREE.PointLight(0xeeeefe, 2, 300, 1.5);
-	lights[0].position.set(45, 15, 105);
-
-	lights[1] = new THREE.PointLight(0x423e6a, 1, 1200, 2);
-	lights[1].position.set(-90, 10, 120);
-
-	lights[2] = new THREE.PointLight(0x513c1f, 1, 300, 3);
-	lights[2].position.set(40, -40, 80);
-
-	for(var i = 0; i < lights.length; i++) {
-		scene.add(lights[i]);
-	}
-
-	var planetMaterial = new THREE.MultiMaterial([
-		new THREE.MeshPhongMaterial({
-			morphTargets: true,
-			shininess: 30,
-			color: 0x526464,
-			side: THREE.DoubleSide,
-			shading: THREE.FlatShading
-		}),
-		new THREE.MeshPhongMaterial({
-			transparent: true,
-			morphTargets: true,
-			shininess: 30,
-			color: 0x526464,
-			opacity: 0,
-			side: THREE.DoubleSide,
-			shading: THREE.FlatShading
-		})
-	]);
-	var planetGeometry = new THREE.IcosahedronGeometry(60, 3);
-	var implodedVertices = [];
-	var cubeVertices = [];
-	for(var i = 0; i < planetGeometry.vertices.length; i++) {
-		implodedVertices[i] = planetGeometry.vertices[i].clone().setLength(0.01);
-		planetGeometry.vertices[i].x += Math.random() * 6 - 3;
-		planetGeometry.vertices[i].y += Math.random() * 6 - 3;
-		planetGeometry.vertices[i].setLength(59 + Math.random() * 2);
-		cubeVertices[i] = planetGeometry.vertices[i].clone().clampScalar(-30, 30);
-	}
-	for(var i = 0; i < planetGeometry.faces.length; i++) {
-		planetGeometry.faces[i].materialIndex = 0; //Math.round(Math.random());
-	}
-	planetGeometry.morphTargets.push({"name": "implode", "vertices": implodedVertices});
-	planetGeometry.morphTargets.push({"name": "cube", "vertices": cubeVertices});
-	var planet = new THREE.Mesh(planetGeometry, planetMaterial);
-	scene.add(planet);
-
-	var satellites = [];
-	var satelliteMaterial = new THREE.MeshPhongMaterial({
-		shininess: 30,
-		color: 0x526464,
-		side: THREE.DoubleSide,
-		shading: THREE.FlatShading
+	var material = new THREE.MeshNormalMaterial({side: THREE.DoubleSide, morphTargets: true});
+	var geometry;
+	var loader = new THREE.OBJLoader();
+	loader.load("img/objects/crane.obj", function(object) {
+		console.log(object);
+		geometry = new THREE.Geometry().fromBufferGeometry(object.children[0].geometry);
 	});
-	for(var i = 0; i < 300; i++) {
-		var radius = Math.max(0.2, Math.pow(Math.random(), 2) * 3);
-		var detail = radius > 2.5 ? 1 : 0;
-		var satelliteGeometry = new THREE.IcosahedronGeometry(radius, detail);
-		if(radius > 2.5) {
-			displaceSatelliteGeometry(satelliteGeometry);
-		}
-		var positionR = 110 - Math.random() * 30;
-		var c = positionR * Math.cos(i * Math.PI / 150);
-		var s = positionR * Math.sin(i * Math.PI / 150);
-		satelliteGeometry.applyMatrix(new THREE.Matrix4().makeRotationFromEuler(new THREE.Euler(Math.random(), Math.random(), Math.random(), "XYZ")));
-		satelliteGeometry.applyMatrix(new THREE.Matrix4().makeTranslation(c, s, Math.random() * 30 - 15));
-		var satellite = new THREE.Mesh(satelliteGeometry, satelliteMaterial);
-		satellite.orbitSpeed = 0.3 / radius;
-		scene.add(satellite);
-		satellites.push(satellite);
-	}
+
+	loader.load("img/objects/cube.obj", function(object) {
+		var cubeVertices = new THREE.Geometry().fromBufferGeometry(object.children[0].geometry).vertices;
+		geometry.morphTargets.push({"name": "cube", "vertices": cubeVertices});
+		window.model = new THREE.Mesh(geometry, material);
+		scene.add(window.model);
+		loaded = true;
+	});
 
 	function render() {
 		requestAnimFrame(render);
 		renderer.render(scene, camera);
+		controls.update();
 
-		var delta = clock.getDelta();
-		planet.rotation.z += delta * animation.planetRotation / 5;
-		for(var i = 0; i < satellites.length; i++) {
-			satellites[i].rotation.z += satellites[i].orbitSpeed * delta * animation.satelliteOrbit;
+		if(loaded) {
+			var delta = clock.getDelta();
+			window.model.rotation.y += delta;
 		}
 	}
 
 	render();
 
 	$(window).resize(function() {
-		renderer.setSize(window.innerWidth, window.innerHeight);
-		camera.aspect = window.innerWidth / window.innerHeight;
+		width = Math.min(window.innerWidth, 100);
+		height = Math.min(window.innerHeight, 100);
+		renderer.setSize(width, height);
+		camera.aspect = width / height;
 		camera.updateProjectionMatrix();
-		factor = window.innerWidth < 768 ? 1.5 : 1;
 	});
 });
