1950eb46f333fdfc39e1d702b88f2c9058689cbe
DarkAce65 <tvdood99@gmail.com>
2017-01-05 22:38:45 -0500

    Animate box and camera
    Former-commit-id: d98d8db1d3fe72afd69dcf20c79df66fa93baa3d

--- script.js
+++ script.js
@@ -26,37 +26,42 @@ $(function() {
 	var controls = new THREE.TrackballControls(camera, renderer.domElement);
 	camera.lookAt(scene.position);
 
-	var ambient = new THREE.AmbientLight(0x274466);
+	var ambient = new THREE.AmbientLight(0xaaaaaa);
 	scene.add(ambient);
 
-	window.pointlight = new THREE.PointLight(0x5e85b4);
-	pointlight.position.set(100, 150, 50);
+	window.pointlight = new THREE.PointLight(0xffffff);
+	pointlight.position.set(100, 100, 250);
 	pointlight.add(new THREE.LineSegments(new THREE.WireframeGeometry(new THREE.SphereBufferGeometry(2)), new THREE.LineBasicMaterial({color: 0x5e85b4})));
 	scene.add(pointlight);
 
-	var faceMaterial = new THREE.MeshPhongMaterial({color: 0x333333, emissive: 0xe91916, side: THREE.DoubleSide, shading: THREE.FlatShading});
 	window.uniforms = THREE.UniformsUtils.merge([
 		THREE.UniformsLib["lights"],
-		{u_time: {type: "f", value: 0}}
+		{
+			u_noiseColor: {type: "f", value: 0},
+			u_time: {type: "f", value: 0},
+			u_multiplier: {type: "f", value: 0}
+		}
 	]);
 	var shaderMaterial = new THREE.ShaderMaterial({
+		transparent: true,
 		lights: true,
 		uniforms: uniforms,
 		vertexShader: document.getElementById("vertexShader").textContent,
 		fragmentShader: document.getElementById("fragmentShader").textContent,
 		side: THREE.DoubleSide
 	});
+	var faceMaterial = new THREE.MeshPhongMaterial({color: 0xf72f35, side: THREE.DoubleSide, shading: THREE.FlatShading});
 
 	window.box = new THREE.Object3D();
 	var top = new THREE.Mesh(new THREE.PlaneBufferGeometry(100, 100, 100, 100), shaderMaterial);
-	top.position.y = 0.5;
+	top.position.y = 1;
 	top.rotation.x = Math.PI / 2;
 	var base = new THREE.Mesh(new THREE.PlaneBufferGeometry(100, 100), faceMaterial);
-	base.position.y = -0.5;
+	base.position.y = -1;
 	base.rotation.x = Math.PI / 2;
 	box.add(top);
 	box.add(base);
-	var sideGeometry = new THREE.PlaneBufferGeometry(100, 1);
+	var sideGeometry = new THREE.PlaneBufferGeometry(100, 2);
 	for(var i = 0; i < 4; i++) {
 		var f = new THREE.Mesh(sideGeometry, faceMaterial);
 		var angle = i * Math.PI / 2;
@@ -66,15 +71,16 @@ $(function() {
 	}
 
 	box.scale.set(0.01, 1, 0.01);
+	box.position.x = -50;
 	scene.add(box);
-	TweenLite.to(box.scale, 1, {x: 1, ease: Power3.easeOut});
-	TweenLite.to(camera.position, 2, {x: 80, y: 70, z: 225, ease: Power2.easeInOut, delay: 1});
-	TweenLite.to(controls.target, 2, {x: 0, y: 0, z: 0, ease: Power2.easeOut, onUpdate: function() {camera.lookAt(controls.target);}, delay: 1});
-	TweenLite.to(camera.up, 1.75, {x: 0, y: 1, z: 0, delay: 1});
-	TweenLite.to(box.scale, 1, {z: 1, ease: Power3.easeOut, delay: 3});
-
-	var axisHelper = new THREE.AxisHelper(100);
-	scene.add(axisHelper);
+	TweenLite.to(box.position, 2, {x: 0, ease: Expo.easeInOut});
+	TweenLite.to(box.scale, 2, {x: 1, ease: Expo.easeInOut});
+	TweenLite.to(uniforms.u_noiseColor, 2, {value: 1, delay: 2});
+	TweenLite.to(uniforms.u_multiplier, 2, {value: 1, delay: 2});
+	TweenLite.to(camera.position, 2, {x: 170, y: 70, z: 170, onUpdate: function() {camera.lookAt(controls.target);}, delay: 3});
+	TweenLite.to(controls.target, 2, {x: 0, y: 0, z: 0, delay: 3});
+	TweenLite.to(camera.up, 1.75, {x: 0, y: 1, z: 0, delay: 3});
+	TweenLite.to(box.scale, 1, {z: 1, delay: 3});
 
 	// if(Cookies.get("animated")) {
 	// 	timeline.seek("menu+=1", false);
