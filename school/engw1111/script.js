976cc92666e10e5ab1b8992e6e55581a146168a7
DarkAce65 <tvdood99@gmail.com>
2017-02-07 14:43:52 -0500

    Fix for strict mode

--- school/engw1111/script.js
+++ school/engw1111/script.js
@@ -23,7 +23,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
 	function update(source) {
 		function diagonal(s, d) { // Creates a curved (diagonal) path from parent to the child nodes
-			path = `M ${s.y} ${s.x}
+			var path = `M ${s.y} ${s.x}
 			C ${(s.y + d.y) / 2} ${s.x},
 			${(s.y + d.y) / 2} ${d.x},
 			${d.y} ${d.x}`;
