e907c0478f6d300e031c7412e28bee01769e2fac
DarkAce65 <tvdood99@gmail.com>
2017-08-31 18:37:33 -0400

    Implement clean reveal animation

--- new/script.js
+++ new/script.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 window.requestAnimFrame =
 	window.requestAnimationFrame ||
@@ -11,32 +11,52 @@ window.requestAnimFrame =
 	};
 
 document.addEventListener('DOMContentLoaded', function(e) {
-	var c = 0;
-	var width = window.innerWidth;
-	var height = window.innerHeight;
+	function fillRadius(x, y) {
+		var w = Math.max(x, canvas.width - x);
+		var h = Math.max(y, canvas.height - y);
 
-	var canvas = document.querySelector('#background');
-	canvas.width = width;
-	canvas.height = height;
-	var ctx = canvas.getContext('2d');
-	ctx.strokeStyle = 'white';
-
-	function render() {
-		ctx.beginPath();
-		ctx.arc(width / 2, height / 2, c % (width / 2), 0, 2 * Math.PI);
-		ctx.stroke();
-
-		c++;
-
-		requestAnimFrame(render);
+		return Math.sqrt(w * w + h * h);
 	}
 
-	render();
+	var canvas = document.querySelector('canvas');
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
+	var ctx = canvas.getContext('2d');
+	ctx.fillStyle = '#121212';
+	ctx.strokeStyle = 'whitesmoke';
+	ctx.lineWidth = 20;
+	var circle = {
+		x: 0,
+		y: 0,
+		r: 0
+	};
+
+	document.addEventListener('click', function(e) {
+		const x = e.clientX;
+		const y = e.clientY;
+		circle.x = x;
+		circle.y = y;
+		const r = fillRadius(x, y);
+
+		const animation = anime({
+			targets: circle,
+			r: r,
+			duration: 2000,
+			easing: 'easeOutQuart',
+			update: function() {
+				ctx.beginPath();
+				ctx.arc(circle.x, circle.y, circle.r, 0, 2 * Math.PI, false);
+				ctx.fill();
+				ctx.strokeRect(0, 0, canvas.width, canvas.height);
+			},
+			complete: function() {
+				document.body.classList.remove('invisible');
+			}
+		});
+	}, {once: true});
 
 	window.addEventListener('resize', function(e) {
-		width = window.innerWidth;
-		height = window.innerHeight;
-		canvas.width = width;
-		canvas.height = height;
+		canvas.width = window.innerWidth;
+		canvas.height = window.innerHeight;
 	});
 });
