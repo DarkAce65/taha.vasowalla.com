dbdec8f1e9522c9651e3becee35fd09b1c9145e3
DarkAce65 <tvdood99@gmail.com>
2018-01-03 17:48:05 -0500

    Add Babel and define package versions

--- new/gulpfile.js
+++ new/gulpfile.js
@@ -1,9 +1,15 @@
 const gulp = require('gulp');
+const rename = require('gulp-rename');
+const sourcemaps = require('gulp-sourcemaps');
+
+const babel = require('gulp-babel');
 const sass = require('gulp-sass');
 const bourbon = require('bourbon').includePaths;
-const sourcemaps = require('gulp-sourcemaps');
 const autoprefixer = require('gulp-autoprefixer');
 
+const babelOptions = {
+    presets: ['@babel/env']
+};
 const sassOptions = {
     includePaths: ['colors'].concat(bourbon)
 };
@@ -11,15 +17,26 @@ const autoprefixerOptions = {
     browsers: ['last 2 versions', '> 5%']
 };
 
+gulp.task('js', function () {
+    return gulp.src(['**/script.js', '!node_modules/**/*.js'])
+        .pipe(sourcemaps.init())
+        .pipe(babel(babelOptions))
+        .pipe(sourcemaps.write('maps'))
+        .pipe(rename(function(path) {
+            if(path.extname === '.js') {
+                path.extname = '.dist.js';
+            }
+        }))
+        .pipe(gulp.dest('.'));
+});
+
 gulp.task('scss', function () {
     return gulp.src(['**/*.scss', '!node_modules/**/*.scss'])
         .pipe(sourcemaps.init())
         .pipe(sass(sassOptions).on('error', sass.logError))
-        .pipe(sourcemaps.write())
         .pipe(autoprefixer(autoprefixerOptions))
-        .pipe(gulp.dest(function(file) {
-            return file.base;
-        }));
+        .pipe(sourcemaps.write('maps'))
+        .pipe(gulp.dest('.'));
 });
 
-gulp.task('default', ['scss']);
+gulp.task('default', ['js', 'scss']);
