bec9f54c8534fd70dc87a06bd5137558686b7946
DarkAce65 <tvdood99@gmail.com>
2018-01-05 17:32:34 -0500

    Replace var with let and const

--- new/script.js
+++ new/script.js
@@ -10,14 +10,14 @@ window.requestAnimFrame =
 
 document.addEventListener('DOMContentLoaded', function(e) {
     function displaceSatelliteGeometry(satelliteGeometry) {
-        for(var i = 0; i < satelliteGeometry.vertices.length; i++) {
-            var v = satelliteGeometry.vertices[i].clone().setLength(0.4);
+        for(let i = 0; i < satelliteGeometry.vertices.length; i++) {
+            const v = satelliteGeometry.vertices[i].clone().setLength(0.4);
             satelliteGeometry.vertices[i].setLength(3 + Math.abs(simplex.noise3D(v.x, v.y, v.z)) * 2);
         }
     }
 
     function setCamera() {
-        var f = 1;
+        let f = 1;
         if(window.innerWidth < 500) {
             f = 2;
         }
@@ -29,12 +29,12 @@ document.addEventListener('DOMContentLoaded', function(e) {
         camera.lookAt(scene.position);
     }
 
-    var simplex = new SimplexNoise();
-    var animation = { planetRotation: 1, satelliteOrbit: 1 };
+    const simplex = new SimplexNoise();
+    const animation = { planetRotation: 1, satelliteOrbit: 1 };
 
-    var clock = new THREE.Clock();
+    const clock = new THREE.Clock();
     window.scene = new THREE.Scene();
-    var renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true });
+    const renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true });
     document.querySelector('#rendererContainer').appendChild(renderer.domElement);
     renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.setPixelRatio(window.devicePixelRatio);
@@ -42,7 +42,7 @@ document.addEventListener('DOMContentLoaded', function(e) {
     window.camera = new THREE.PerspectiveCamera(35, window.innerWidth / window.innerHeight, 0.1, 10000);
     setCamera();
 
-    var lights = [];
+    const lights = [];
 
     lights[0] = new THREE.PointLight(0xeeeefe, 2, 300, 1.5);
     lights[0].position.set(45, 15, 105);
@@ -53,18 +53,18 @@ document.addEventListener('DOMContentLoaded', function(e) {
     lights[2] = new THREE.PointLight(0x513c1f, 1, 300, 3);
     lights[2].position.set(40, -40, 80);
 
-    for(var i = 0; i < lights.length; i++) {
+    for(let i = 0; i < lights.length; i++) {
         scene.add(lights[i]);
     }
 
-    var planetMaterial = new THREE.MeshPhongMaterial({
+    const planetMaterial = new THREE.MeshPhongMaterial({
         shininess: 30,
         color: 0x526464,
         side: THREE.DoubleSide,
         flatShading: true
     });
-    var planetGeometry = new THREE.IcosahedronGeometry(50, 3);
-    for(var i = 0; i < planetGeometry.vertices.length; i++) {
+    const planetGeometry = new THREE.IcosahedronGeometry(50, 3);
+    for(let i = 0; i < planetGeometry.vertices.length; i++) {
         planetGeometry.vertices[i].x += Math.random() * 6 - 3;
         planetGeometry.vertices[i].y += Math.random() * 6 - 3;
         if(Math.random() < 0.1) {
@@ -77,26 +77,26 @@ document.addEventListener('DOMContentLoaded', function(e) {
     window.planet = new THREE.Mesh(new THREE.BufferGeometry().fromGeometry(planetGeometry), planetMaterial);
     scene.add(planet);
 
-    var satellites = [];
-    var satelliteMaterial = new THREE.MeshPhongMaterial({
+    const satellites = [];
+    const satelliteMaterial = new THREE.MeshPhongMaterial({
         shininess: 30,
         color: 0x526464,
         side: THREE.DoubleSide,
         flatShading: true
     });
-    for(var i = 0; i < 100; i++) {
-        var radius = Math.max(1, Math.pow(Math.random() + 0.2, 2) * 2.5);
-        var detail = radius > 2.5 ? 1 : 0;
-        var satelliteGeometry = new THREE.IcosahedronGeometry(radius, detail);
+    for(let i = 0; i < 100; i++) {
+        const radius = Math.max(1, Math.pow(Math.random() + 0.2, 2) * 2.5);
+        const detail = radius > 2.5 ? 1 : 0;
+        const satelliteGeometry = new THREE.IcosahedronGeometry(radius, detail);
         if(radius > 2.5) {
             displaceSatelliteGeometry(satelliteGeometry);
         }
-        var positionR = 80 + Math.random() * 30;
-        var c = positionR * Math.cos(i * Math.PI / 50);
-        var s = positionR * Math.sin(i * Math.PI / 50);
+        const positionR = 80 + Math.random() * 30;
+        const c = positionR * Math.cos(i * Math.PI / 50);
+        const s = positionR * Math.sin(i * Math.PI / 50);
         satelliteGeometry.applyMatrix(new THREE.Matrix4().makeRotationFromEuler(new THREE.Euler(Math.random(), Math.random(), Math.random(), 'XYZ')));
         satelliteGeometry.applyMatrix(new THREE.Matrix4().makeTranslation(c, s, Math.random() * 30 - 15));
-        var satellite = new THREE.Mesh(new THREE.BufferGeometry().fromGeometry(satelliteGeometry), satelliteMaterial);
+        const satellite = new THREE.Mesh(new THREE.BufferGeometry().fromGeometry(satelliteGeometry), satelliteMaterial);
         satellite.orbitSpeed = 0.3 / radius;
         scene.add(satellite);
         satellites.push(satellite);
@@ -106,9 +106,9 @@ document.addEventListener('DOMContentLoaded', function(e) {
         requestAnimFrame(render);
         renderer.render(scene, camera);
 
-        var delta = clock.getDelta();
+        const delta = clock.getDelta();
         planet.rotation.z += delta * animation.planetRotation / 5;
-        for(var i = 0; i < satellites.length; i++) {
+        for(let i = 0; i < satellites.length; i++) {
             satellites[i].rotation.z += satellites[i].orbitSpeed * delta * animation.satelliteOrbit;
         }
     }
