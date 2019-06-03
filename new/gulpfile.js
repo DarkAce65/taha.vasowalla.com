4fe2468295061003ef40eafaf51dabb517ef908b
DarkAce65 <tvdood99@gmail.com>
2019-06-02 20:19:22 -0400

    Add pug compilation task to gulpfile

--- new/gulpfile.js
+++ new/gulpfile.js
@@ -1,13 +1,19 @@
 const gulp = require('gulp');
+const path = require('path');
 
+const plumber = require('gulp-plumber');
+const chalk = require('chalk');
+const log = require('fancy-log');
 const using = require('gulp-using');
 
+const pug = require('gulp-pug');
 const eslint = require('gulp-eslint');
 const babel = require('gulp-babel');
 const sass = require('gulp-sass');
 sass.compiler = require('sass');
 const autoprefixer = require('gulp-autoprefixer');
 
+const pugSources = 'src/**/*.pug';
 const scriptSources = 'src/**/*.js';
 const scssPartials = 'src/**/_*.scss';
 const styleSources = ['src/**/*.scss', `!${scssPartials}`];
@@ -22,6 +28,32 @@ const sassOptions = {
 const autoprefixerOptions = {
   browsers: ['last 2 versions', '> 5%'],
 };
+
+const compileHTML = () =>
+  gulp
+    .src(pugSources, { since: gulp.lastRun(compileHTML) })
+    .pipe(plumber())
+    .pipe(
+      pug({ pretty: true }).on('error', error => {
+        if (error.path) {
+          log(
+            `${chalk.red(error.name)} in ${chalk.magenta(path.relative(process.cwd(), error.path))}\n`,
+            error.message
+          );
+        } else if (error.code && error.filename) {
+          log(
+            `${chalk.red(error.code)} in ${chalk.magenta(path.relative(process.cwd(), error.filename))}\nMessage: ${
+              error.msg
+            }`
+          );
+        } else {
+          log(error.message);
+        }
+      })
+    )
+    .pipe(plumber.stop())
+    .pipe(gulp.dest('dist'))
+    .pipe(using({ prefix: 'Writing', filesize: true }));
 
 const lintScripts = () =>
   gulp
@@ -49,8 +81,7 @@ const compileStyles = ({ compileAll } = {}) => {
         sourcemaps: true,
         since: compileAll ? null : gulp.lastRun(compile),
       })
-      .pipe(sass.sync(sassOptions))
-      .on('error', sass.logError)
+      .pipe(sass.sync(sassOptions).on('error', sass.logError))
       .pipe(autoprefixer(autoprefixerOptions))
       .pipe(gulp.dest('dist', { sourcemaps: 'maps' }))
       .pipe(using({ prefix: 'Writing', filesize: true }));
@@ -58,6 +89,9 @@ const compileStyles = ({ compileAll } = {}) => {
 
   return compile;
 };
+
+const watchHTML = () => gulp.watch(pugSources, { ignoreInitial: false }, compileHTML);
+watchHTML.displayName = 'watch:html';
 
 const watchScripts = () => gulp.watch(scriptSources, { ignoreInitial: false }, compileScripts);
 watchScripts.displayName = 'watch:scripts';
@@ -71,6 +105,6 @@ watchSCSSPartials.displayName = 'watch:scssPartials';
 exports.html = compileHTML;
 exports.scripts = compileScripts;
 exports.styles = compileStyles();
-exports.watch = gulp.parallel(watchScripts, watchStyles, watchSCSSPartials);
+exports.watch = gulp.parallel(watchHTML, watchScripts, watchStyles, watchSCSSPartials);
 
 exports.default = gulp.parallel(compileHTML, compileScripts, compileStyles());
