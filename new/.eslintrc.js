96d5afcd6081ae4ca5e090f86e012a79d059f81f
DarkAce65 <tvdood99@gmail.com>
2019-10-30 00:17:07 -0400

    Add no-duplicate-imports rule

--- new/.eslintrc.js
+++ new/.eslintrc.js
@@ -4,18 +4,13 @@ module.exports = {
   parser: 'babel-eslint',
   plugins: ['prettier'],
   extends: ['eslint:recommended', 'prettier', 'plugin:prettier/recommended'],
-  env: {
-    browser: true,
-    node: true,
-    es6: true,
-  },
-  parserOptions: {
-    ecmaVersion: 6,
-  },
+  env: { browser: true, node: true, es6: true },
+  parserOptions: { ecmaVersion: 6 },
   rules: {
     'prettier/prettier': ['warn', prettierConfig],
     eqeqeq: 'warn',
     'no-console': 'off',
+    'no-duplicate-imports': 'warn',
     'no-shadow': 'error',
     'no-unused-vars': 'warn',
     'no-var': 'error',
