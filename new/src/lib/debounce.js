822298aa6b1943296a8212cd0cb0b9886ccb7925
DarkAce65 <tvdood99@gmail.com>
2020-02-29 21:04:11 -0500

    Add now function to immediately call debounced function

--- new/src/lib/debounce.js
+++ new/src/lib/debounce.js
@@ -1,8 +1,14 @@
 export default (func, { delay = 500 } = {}) => {
   let timeout = null;
 
-  return function(...args) {
+  const debouncedFunction = function(...args) {
     clearTimeout(timeout);
     timeout = setTimeout(() => func.call(this, ...args), delay);
   };
+  debouncedFunction.now = function(...args) {
+    clearTimeout(timeout);
+    func.call(this, ...args);
+  };
+
+  return debouncedFunction;
 };
