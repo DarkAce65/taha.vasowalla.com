cbdcd688d6105649cf6e041a5b6eea2382cee629
DarkAce65 <tvdood99@gmail.com>
2020-03-16 22:39:58 -0400

    Add parent option to getEl utility

--- new/src/lib/getEl.js
+++ new/src/lib/getEl.js
@@ -2,16 +2,23 @@ const isString = value => typeof value === 'string';
 
 const isNode = obj => obj !== null && typeof obj === 'object' && obj.nodeType >= 1;
 
-const getEl = selector =>
-  isString(selector) ? document.querySelector(selector) : isNode(selector) ? selector : null;
+const getEl = (selector, parent = document) =>
+  isString(selector) ? getEl(parent).querySelector(selector) : isNode(selector) ? selector : null;
 
-export const getElOrThrow = selector => {
-  const el = getEl(selector);
+export const getElOrThrow = (selector, parent) => {
+  let parentEl;
+  if (parent) {
+    parentEl = getElOrThrow(parent);
+  }
+
+  const el = getEl(selector, parentEl);
   if (!el) {
-    throw new Error(`Selector matched no element: ${selector}`);
+    throw new Error(`Selector "${selector}" matched no element`);
   }
 
   return el;
 };
 
 export default getEl;
+window.getEl = getEl;
+window.getElOrThrow = getElOrThrow;
