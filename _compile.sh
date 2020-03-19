ccd95e5271d0790311ef768dd37057eb98435c21
DarkAce65 <tvdood99@gmail.com>
2020-03-19 01:13:58 -0400

    Remove dependency on bourbon

--- _compile.sh
+++ _compile.sh
@@ -6,18 +6,9 @@ if ! type "sass" > /dev/null; then
 	exit
 fi
 
-if [[ ! -d "bourbon" ]]; then
-	echo "Error: Bourbon mixins not installed."
-	if ! type "bourbon" > /dev/null; then
-		echo "Install bourbon via 'gem install bourbon'"
-	fi
-	echo "Run 'bourbon install' in this directory"
-	exit
-fi
-
 echo "Compiling global.scss..."
 sass --style $style --update global.scss:global.css
 echo "Compiling colors directory..."
 sass --style $style --update colors:colors
 echo "Compiling all subdirectories..."
-sass --style $style --update new:new about:about origami:origami photography:photography ventures:ventures school:school tools:tools visual:visual games:games experiments:experiments
+sass --style $style --update about:about origami:origami photography:photography ventures:ventures school:school tools:tools visual:visual games:games experiments:experiments
