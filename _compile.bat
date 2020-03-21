a6c5d5a14ff74c39a589cfc779a481c831cfb130
DarkAce65 <tvdood99@gmail.com>
2020-03-20 20:50:14 -0400

    Update batch file

--- _compile.bat
+++ _compile.bat
@@ -17,20 +17,9 @@ if %ERRORLEVEL% neq 0 (
 )
 for /f "tokens=*" %%i in ('where sass') do if not defined SASS_COMMAND set SASS_COMMAND=%%i
 
-if not exist bourbon (
-	echo Error: Bourbon mixins not installed.
-	where bourbon > nul 2>&1
-	if %ERRORLEVEL% neq 0 (
-		echo Install bourbon: 'gem install bourbon'
-	)
-	echo Run 'bourbon install' in this directory
-	pause
-	exit
-)
-
 echo Compiling global.scss...
 ruby %SASS_COMMAND% --style %STYLE% --update global.scss:global.css
 echo Compiling colors directory...
 ruby %SASS_COMMAND% --style %STYLE% --update colors:colors
 echo Compiling all subdirectories...
-ruby %SASS_COMMAND% --style %STYLE% --update new:new about:about origami:origami photography:photography ventures:ventures school:school tools:tools visual:visual games:games experiments:experiments
+ruby %SASS_COMMAND% --style %STYLE% --update about:about origami:origami photography:photography ventures:ventures school:school tools:tools visual:visual games:games experiments:experiments
