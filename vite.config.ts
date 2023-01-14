4eeec5c8c8262a469f6d6a3277b0514baa1052c9
DarkAce65 <tvdood99@gmail.com>
2023-01-14 15:25:35 -0500

    Configure entrypoints via object

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,47 @@
-import { resolve } from 'path';
+import path from 'path';
 
 import { SafeString, escapeExpression } from 'handlebars';
 import { Plugin, defineConfig } from 'vite';
 import checker from 'vite-plugin-checker';
 import handlebarsPlugin from 'vite-plugin-handlebars';
 
-const srcDir = resolve(__dirname, 'src');
+const srcDir = path.resolve(__dirname, 'src');
+
+const pages = {
+  index: { dir: '.', entry: './script.ts' },
+  about: { dir: 'about', entry: './script.ts' },
+  // animation: { dir: 'random/animation', entry: './script.js' },
+  // cards: { dir: 'visual/cards', entry: './script.ts' },
+  // chemistry: { dir: 'projects/chemistry', entry: './script.js' },
+  // driving: { dir: 'projects/driving', entry: './script.ts' },
+  // fireball: { dir: 'random/fireball', entry: './script.js' },
+  // hangman: { dir: 'games/hangman', entry: './script.ts' },
+  // minesweeper: { dir: 'games/minesweeper', entry: './script.ts' },
+  // shaders: { dir: 'random/shaders', entry: './script.js' },
+  // testing: { dir: 'random/testing', entry: './script.ts' },
+  // ultimatettt: { dir: 'games/ultimatettt', entry: './script.js' },
+  webaudio2d: { dir: 'visual/webaudio2d', entry: './script.ts' },
+  // webaudio3d: { dir: 'visual/webaudio3d', entry: './script.ts' },
+  // wordsearch: { dir: 'projects/wordsearch', entry: './script.ts' },
+};
+
+const entrypoints = Object.entries(pages).reduce(
+  (acc, [entryName, { dir, entry }]) => ({
+    ...acc,
+    [entryName]: path.join(srcDir, dir, entry),
+    [dir]: path.join(srcDir, dir, 'index.html'),
+  }),
+  {}
+);
 
 export default defineConfig(({ mode }) => ({
+  appType: 'mpa',
   root: srcDir,
+  publicDir: path.resolve(__dirname, 'public'),
   build: {
-    outDir: resolve(__dirname, 'dist'),
+    outDir: path.resolve(__dirname, 'dist'),
     emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        '403': resolve(srcDir, '403.html'),
-        '404': resolve(srcDir, '404.html'),
-        index: resolve(srcDir, 'index.html'),
-        webaudio2d: resolve(srcDir, 'visual/webaudio2d/index.html'),
-        about: resolve(srcDir, 'about/index.html'),
-      },
-    },
+    rollupOptions: { input: entrypoints },
     sourcemap: true,
   },
   optimizeDeps: {
